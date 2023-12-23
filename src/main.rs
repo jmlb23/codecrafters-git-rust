3ccbad301572e35dfb75f891d4f682e1b7deeb47
jmlb23 <rmmenosrf@riseup.net>
2023-12-23 13:43:17 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[allow(unused_imports)]
 use std::env;
+use std::{fs::File, io::{BufReader, BufRead}};
 #[allow(unused_imports)]
 use std::fs;
+use flate2::{self, read::ZlibDecoder};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,19 +11,30 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    if args.get(1).map(|x| x.as_str()).unwrap_or_else(|| "") == "init" {
-        match fs::create_dir(".git")
-                    .and_then(|_| fs::create_dir(".git/objects"))
-                    .and_then(|_| fs::create_dir(".git/refs"))
-                    .and_then(|_| fs::write(".git/HEAD", "ref: refs/heads/master\n")) {
-            Ok(_) => {
-                println!("Initialized git directory")
-            },
-            Err(_) => {
-                println!("Something wrong happened")
-            },
+
+    match args.get(1).map(|x| x.as_str()).unwrap_or_else(|| "") {
+        "init" => {
+            match fs::create_dir(".git")
+                .and_then(|_| fs::create_dir(".git/objects"))
+                .and_then(|_| fs::create_dir(".git/refs"))
+                .and_then(|_| fs::write(".git/HEAD", "ref: refs/heads/master\n"))
+            {
+                Ok(_) => {
+                    println!("Initialized git directory")
+                }
+                Err(_) => {
+                    println!("Something wrong happened")
+                }
+            }
         }
-    } else {
-         println!("unknown command: {}", args[1])
+        "cat-file" => {
+            let file = args.last().map(|s| s.as_str()).unwrap_or_else(|| "");
+            let stream = File::open(format!(".git/objects/{}",file)).unwrap();
+            let buff_reader = BufReader::new(ZlibDecoder::new(stream));
+            buff_reader.lines().for_each(|line| println!("{}", line.unwrap()));
+        }
+        _ => {
+            println!("unknown command: {}", args[1])
+        }
     }
 }
